1d541008beffa296dccee3ba197db5047524823f
ggslove <ggslove@gmail.com>
2021-11-30 17:56:07 +0800

    加入 rocketMq代码案例

---
+++ rocketmq-demo/rocketmq-provider/src/main/java/yu/rocketmqprovider/RocketmqProviderApp.java
@@ -0,0 +1,11 @@
+package yu.rocketmqprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqProviderApp {
+    public static void main(String args[]) {
+        SpringApplication.run(RocketmqProviderApp.class, args);
+    }
+}
